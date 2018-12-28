5a2541f11eac50c7bf7ab2d83be188e2e4a1e9d2
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2018-12-28 11:34:19 -0200

    (Java parte 2: Introdução à Orientação a Objetos) Introdução à Orientação a Objetos: Instanciação, atributos e referências - Criada nova classe

---
+++ Java parte 2 - Introdução à Orientação a Objetos/workspace-alura-java-parte-2/ByteBank/src/CriaConta.java
@@ -0,0 +1,9 @@
+public class CriaConta {
+	public static void main(String[] args) {
+		// Instancia um Objeto do tipo Conta;
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		
+		System.out.println(primeiraConta.saldo);
+	}
+}
