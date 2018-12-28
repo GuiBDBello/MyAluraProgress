133b7dd934db6d98b34be115c5492cfeabb73775
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2018-12-28 17:48:51 -0200

    (Java parte 2: Introdução à Orientação a Objetos) Definindo comportamento: Nosso primeiro método - Criada nova classe

--- Java parte 2 - Introdução à Orientação a Objetos/workspace-alura-java-parte-2/ByteBank/src/Conta.java
+++ Java parte 2 - Introdução à Orientação a Objetos/workspace-alura-java-parte-2/ByteBank/src/Conta.java
@@ -3,4 +3,9 @@ public class Conta {
 	int agencia = 42;
 	int numero;
 	String titular;
+	
+	void deposita(double valor) {
+		this.saldo += valor;
+		System.out.println("Foi depositado R$" + valor + ". Seu saldo atual é R$" + this.saldo);
+	}
 }
