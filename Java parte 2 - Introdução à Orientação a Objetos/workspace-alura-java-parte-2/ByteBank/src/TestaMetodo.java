2c7e29f75c24e0dfc0bb0cdd26e95be6134f8d55
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2018-12-28 20:36:05 -0200

    (Java parte 2: Introdução à Orientação a Objetos) Definindo comportamento: Métodos com referência e mais retorno

--- Java parte 2 - Introdução à Orientação a Objetos/workspace-alura-java-parte-2/ByteBank/src/TestaMetodo.java
+++ Java parte 2 - Introdução à Orientação a Objetos/workspace-alura-java-parte-2/ByteBank/src/TestaMetodo.java
@@ -2,6 +2,7 @@ public class TestaMetodo {
 	public static void main(String[] args) {
 		Conta conta = new Conta();
 		boolean conseguiuRetirar;
+		boolean conseguiuTransferir;
 		
 		conta.saldo = 100;
 		System.out.println(conta.saldo);
@@ -13,5 +14,21 @@ public class TestaMetodo {
 		conseguiuRetirar = conta.saca(100);
 		System.out.println(conta.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta conta2 = new Conta();
+		conta2.deposita(1000);
+		
+		conseguiuTransferir = conta2.transfere(300, conta);
+		if (conseguiuTransferir) {
+			System.out.println("Transferência efetuada!");
+		} else {
+			System.out.println("Transferência falhou!");
+		}
+		System.out.println(conta2.saldo);
+		System.out.println(conta.saldo);
+
+		System.out.println(conta.titular);
+		conta.titular = "Guilherme Brunetto Dall Bello";
+		System.out.println(conta.titular);
 	}
 }
