7a4f58434f05909cf65317473cb0a288a0e13698
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-02 10:51:06 -0200

    (Java parte 3: Entendendo herança e interface) Interfaces: Mais uma classe abstrata - Criadas novas classes

--- Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Gerente.java
+++ Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Gerente.java
@@ -1,23 +1,5 @@
-// Gerente é um Funcionário, Gerente herda da Classe Funcionario
-public class Gerente extends Funcionario {
-
-	private int senha;
-
-	public int getSenha() {
-		return senha;
-	}
-
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-
-	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+// Gerente é um Funcionário Autenticável, Gerente herda da Classe FuncionarioAutenticavel
+public class Gerente extends FuncionarioAutenticavel {
 
 	public double getBonificacao() {
 		System.out.println("Chamando o método de Bonificação do Gerente.");
