a21dd7c19ff1b664c01486d6bd48f27b9dda2edb
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-02 14:32:09 -0200

    (Java parte 3: Entendendo herança e interface) Interfaces: Completando o sistema

--- Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Gerente.java
+++ Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Gerente.java
@@ -1,8 +1,25 @@
-// Gerente é Funcionário, Gerente herda da Classe Funcionario
-public class Gerente extends Funcionario {
+// Gerente é Funcionário, herda da Classe Funcionario
+// Gerente é Autenticável, assina o contrato Autenticavel
+public class Gerente extends Funcionario implements Autenticavel {
 
+	private int senha;
+	
 	public double getBonificacao() {
 		System.out.println("Chamando o método de Bonificação do Gerente.");
 		return super.getSalario();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/TesteGerente.java
+++ Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/TesteGerente.java
@@ -2,7 +2,6 @@ public class TesteGerente {
 	public static void main(String[] args) {
 		
 		Gerente g1 = new Gerente();
-		
 		g1.setNome("Guilherme");
 		g1.setCpf("123.456.789-10");
 		g1.setSalario(3000);
@@ -11,12 +10,12 @@ public class TesteGerente {
 		System.out.println(g1.getCpf());
 		System.out.println(g1.getSalario());
 		
-//		g1.setSenha(1234);
-//		boolean autenticou = g1.autentica(1234);
-//		
-//		System.out.println(autenticou);
+		g1.setSenha(1234);
+		boolean autenticou = g1.autentica(1234);
+		System.out.println(autenticou);
 		
 		System.out.println(g1.getBonificacao());
 		
+		Autenticavel g2 = new Gerente();
 	}
 }
