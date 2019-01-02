4c212dcd375265dd11ea22e636faf085e43b712b
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-02 15:57:36 -0200

    (Java parte 3: Entendendo herança e interface) Praticando herança e interfaces: Revendo a composição - Criada nova classe

--- Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Administrador.java
+++ Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Administrador.java
@@ -2,8 +2,12 @@
 // Administrador é Autenticável, assina o contrato Autenticavel
 public class Administrador extends Funcionario implements Autenticavel {
 
-	private int senha;
-	
+	private AutenticacaoUtil autenticador;
+
+	public Administrador() {
+		 this.autenticador = new AutenticacaoUtil();
+	}
+
 	@Override
 	public double getBonificacao() {
 		System.out.println("Chamando o método de Bonificação do Administrador.");
@@ -12,15 +16,11 @@ public class Administrador extends Funcionario implements Autenticavel {
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 	}
 }
--- Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Gerente.java
+++ Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Gerente.java
@@ -2,8 +2,12 @@
 // Gerente é Autenticável, assina o contrato Autenticavel
 public class Gerente extends Funcionario implements Autenticavel {
 
-	private int senha;
-	
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		 this.autenticador = new AutenticacaoUtil();
+	}
+
 	public double getBonificacao() {
 		System.out.println("Chamando o método de Bonificação do Gerente.");
 		return super.getSalario();
@@ -11,15 +15,11 @@ public class Gerente extends Funcionario implements Autenticavel {
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 	}
 }
