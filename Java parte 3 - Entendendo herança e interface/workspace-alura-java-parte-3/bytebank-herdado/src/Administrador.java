28d90a92d27f3a5f145a5b887bf5e80a6f6f4dcb
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-02 14:32:09 -0200

    (Java parte 3: Entendendo herança e interface) Interfaces: Completando o sistema

--- Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Administrador.java
+++ Java parte 3 - Entendendo herança e interface/workspace-alura-java-parte-3/bytebank-herdado/src/Administrador.java
@@ -1,9 +1,26 @@
-// Administrador é Funcionario, Administrador herda da Classe Funcionário
-public class Administrador extends Funcionario {
+// Administrador é Funcionario, herda da Classe Funcionário
+// Administrador é Autenticável, assina o contrato Autenticavel
+public class Administrador extends Funcionario implements Autenticavel {
 
+	private int senha;
+	
 	@Override
 	public double getBonificacao() {
 		System.out.println("Chamando o método de Bonificação do Administrador.");
 		return 50;
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
