fc0bceccf7d9ce1bf55aece8a4fad1188c134fc8
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-02-03 23:39:17 -0200

    (JavaScript: Programando na linguagem da web) Variáveis e Operadores: Operadores Lógicos

--- JavaScript - Programando na linguagem da web/introducao-javascript/js/principal.js
+++ JavaScript - Programando na linguagem da web/introducao-javascript/js/principal.js
@@ -10,8 +10,26 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 1 || peso >= 1000) {
+	console.log("Peso inválido!");
+	pesoEhValido = false;
+	tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0.1 || altura >= 3.00) {
+	console.log("Altura inválida!");
+	alturaEhValida = false;
+	tdImc.textContent = "Altura inválido!";
+}
+
+if (pesoEhValido && alturaEhValida) {
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc;
+}
 
 console.log(paciente);
 console.log(tdPeso);
