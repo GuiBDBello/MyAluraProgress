d441dd9b5a4540b23d0b8f32528eedeeecc5d356
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-05 13:18:18 -0300

    (JavaScript: Programando na linguagem da web) AjaxBuscando pacientes com AJAX: Introdução ao AJAX

---
+++ JavaScript - Programando na linguagem da web/introducao-javascript/js/buscar-pacientes.js
@@ -0,0 +1,15 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function() {
+	console.log("Buscando...");
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+	xhr.addEventListener("load", function() {
+		console.log(xhr.responseText);
+	});
+
+	xhr.send();
+});
