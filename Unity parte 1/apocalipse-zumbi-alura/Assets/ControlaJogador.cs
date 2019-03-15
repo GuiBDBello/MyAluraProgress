d2b0a61e0731f9a30aeecfeda048dd84d1f0486a
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-15 17:01:15 -0300

    (Unity parte 1: Criação de um jogo de sobrevivência à zumbis para Web) Cenário e Personagem Jogador: Movimentação por segundo

--- Unity parte 1/apocalipse-zumbi-alura/Assets/ControlaJogador.cs
+++ Unity parte 1/apocalipse-zumbi-alura/Assets/ControlaJogador.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ControlaJogador : MonoBehaviour
 {
+    public float Velocidade = 10;
+
     // Update is called once per frame
     void Update()
     {
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
-        Vector3 direcao = new Vector3(eixoX, 0.0f, eixoZ);
+        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
 
-        transform.Translate(direcao);
+        transform.Translate(direcao * Velocidade * Time.deltaTime);
     }
 }
