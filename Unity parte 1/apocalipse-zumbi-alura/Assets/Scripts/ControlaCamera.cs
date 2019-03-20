80abd199d447c5d6b2eb22513a1dcc56320847b9
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-20 20:15:28 -0300

    (Unity parte 1: Criação de um jogo de sobrevivência à zumbis para Web) Animação, Colisão e Física: Movimentando a câmera

---
+++ Unity parte 1/apocalipse-zumbi-alura/Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour
+{
+
+    public GameObject Jogador;
+
+    Vector3 distanciaCompensar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanciaCompensar = transform.position - Jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Jogador.transform.position + distanciaCompensar;
+    }
+}
