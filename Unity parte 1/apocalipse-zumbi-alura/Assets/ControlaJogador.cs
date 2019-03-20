adb4fa7c54dcca76d1b1b946c5259cc88fe20eb6
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-20 18:46:05 -0300

    (Unity parte 1: Criação de um jogo de sobrevivência à zumbis para Web) Animação, Colisão e Física: Sistema de animações Mecanim

--- Unity parte 1/apocalipse-zumbi-alura/Assets/ControlaJogador.cs
+++ Unity parte 1/apocalipse-zumbi-alura/Assets/ControlaJogador.cs
@@ -12,6 +12,15 @@ public class ControlaJogador : MonoBehaviour
 
         Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
 
+        // Verifica a variável "direcao" para alterar a Animação;
+        if (direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
         transform.Translate(direcao * Velocidade * Time.deltaTime);
     }
 }
