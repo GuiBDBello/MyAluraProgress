39f43bac1aea5fa88a1a89d3f30f65012816efaf
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-20 23:38:08 -0300

    (Unity parte 1: Criação de um jogo de sobrevivência à zumbis para Web) Animação, Colisão e Física: Física na Unity

--- Unity parte 1/apocalipse-zumbi-alura/Assets/Scripts/ControlaJogador.cs
+++ Unity parte 1/apocalipse-zumbi-alura/Assets/Scripts/ControlaJogador.cs
@@ -4,13 +4,18 @@ public class ControlaJogador : MonoBehaviour
 {
     public float Velocidade = 10;
 
+    Vector3 direcao;
+
     // Update is called once per frame
     void Update()
     {
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
-        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+        direcao = new Vector3(eixoX, 0, eixoZ);
+
+        //transform.Translate(direcao * Velocidade * Time.deltaTime);
+        //GetComponent<Rigidbody>().MovePosition(direcao * Velocidade * Time.deltaTime);
 
         // Verifica a variável "direcao" para alterar a Animação;
         if (direcao != Vector3.zero)
@@ -21,6 +26,10 @@ public class ControlaJogador : MonoBehaviour
         {
             GetComponent<Animator>().SetBool("Movendo", false);
         }
-        transform.Translate(direcao * Velocidade * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * Velocidade * Time.deltaTime));
     }
 }
