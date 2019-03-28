ec4f174305cd792687e6f6a1698b1b86f5974584
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-28 20:04:36 -0300

    (Unity parte 2: Iluminação, interface e boas práticas) Interface na Unity: Game Over pela Vida - Adicionado Projeto

--- Unity parte 2/README.md
+++ Unity parte 2/README.md
@@ -8,3 +8,11 @@
 
 - `void TomarDano() { Vida -= 30; }`: método que tira 'Vida' do 'Jogador';
 - `Jogador.GetComponent<ControlaJogador>().TomarDano();`: acessa o Script "ControlaJogador" e executa o método 'TomarDano';
+
+### Atividade 06 - Game Over pela Vida:
+
+#### Script:
+
+- `if (Vida <= 0) { /* Game Over */ }`: gera o "Game Over" quando o 'Jogador' estiver com a 'Vida' menor ou igual a zero;
+- `void TomarDano(int dano) { Vida -= dano }`: diminui a 'Vida' do 'Jogador' baseado no 'dano';
+- `int dano = Random.Range(20, 30);`: define um 'dano' variável que o 'Jogador' receberá;
---
+++ Unity parte 2/apocalipse-zumbi-alura/Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlaJogador : MonoBehaviour
+{
+
+    public float Velocidade = 10;
+    public LayerMask MascaraChao;
+    public GameObject TextoGameOver;
+    public int Vida = 100;
+
+    private Vector3 direcao;
+    private Rigidbody rigidbodyJogador;
+    private Animator animatorJogador;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        rigidbodyJogador = GetComponent<Rigidbody>();
+        animatorJogador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixoX, 0, eixoZ);
+
+        if (direcao != Vector3.zero)
+        {
+            animatorJogador.SetBool("Movendo", true);
+        }
+        else
+        {
+            animatorJogador.SetBool("Movendo", false);
+        }
+
+        if(Vida <= 0)
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                SceneManager.LoadScene("game");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbodyJogador.MovePosition
+            (rigidbodyJogador.position +
+            (direcao * Velocidade * Time.deltaTime));
+
+        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(raio.origin, raio.direction * 100, Color.red);
+
+        RaycastHit impacto;
+
+        if(Physics.Raycast(raio, out impacto, 100, MascaraChao))
+        {
+            Vector3 posicaoMiraJogador = impacto.point - transform.position;
+
+            posicaoMiraJogador.y = transform.position.y;
+
+            Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
+
+            rigidbodyJogador.MoveRotation(novaRotacao);
+        }
+    }
+
+    public void TomarDano(int dano)
+    {
+        Vida -= dano;
+
+        if (Vida <= 0)
+        {
+            Time.timeScale = 0;
+            TextoGameOver.SetActive(true);
+        }
+    }
+}
