2f8e5c78196509d07847e790c82ea82d4b3e3341
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-28 23:24:26 -0300

    (Unity parte 2: Iluminação, interface e boas práticas) Interface na Unity: Fazer a Slider Funcionar

---
+++ Unity parte 2/apocalipse-zumbi-alura/Assets/Scripts/ControlaInterface.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlaInterface : MonoBehaviour
+{
+
+    public Slider SliderVidaJogador;
+
+    private ControlaJogador scriptControlaJogador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scriptControlaJogador = GameObject.FindWithTag("Jogador").GetComponent<ControlaJogador>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AtualizarSliderVidaJogador()
+    {
+        SliderVidaJogador.value = scriptControlaJogador.Vida;
+    }
+}
