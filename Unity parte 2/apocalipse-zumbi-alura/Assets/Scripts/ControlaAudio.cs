71fb216085a6964993fe2956e71c2a01e4e96839
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-30 16:23:28 -0300

    (Unity parte 2: Iluminação, interface e boas práticas) Audio, Cenário e Iluminação: Música no Jogo

---
+++ Unity parte 2/apocalipse-zumbi-alura/Assets/Scripts/ControlaAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaAudio : MonoBehaviour
+{
+
+    public static AudioSource instancia;
+
+    private AudioSource meuAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meuAudioSource = GetComponent<AudioSource>();
+        instancia = meuAudioSource;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
