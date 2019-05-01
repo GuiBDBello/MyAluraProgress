0f5427dd48eb76ec3b6d6b3f14d51e217db42e42
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-05-01 09:32:17 -0300

    Start Unity parte 3: Criando um chefão e refinando o jogo

---
+++ Unity parte 3/apocalipse-zumbi-alura/Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour, IMatavel
+{
+
+    private Vector3 direcao;
+    public LayerMask MascaraChao;
+    public GameObject TextoGameOver;
+    public ControlaInterface scriptControlaInterface;
+    public AudioClip SomDeDano;
+    private MovimentoJogador meuMovimentoJogador;
+    private AnimacaoPersonagem animacaoJogador;
+    public Status statusJogador;
+
+    private void Start()
+    {
+        meuMovimentoJogador = GetComponent<MovimentoJogador>();
+        animacaoJogador = GetComponent<AnimacaoPersonagem>();
+        statusJogador = GetComponent<Status>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixoX, 0, eixoZ);
+
+        animacaoJogador.Movimentar(direcao.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        meuMovimentoJogador.Movimentar(direcao, statusJogador.Velocidade);
+
+        meuMovimentoJogador.RotacaoJogador(MascaraChao);
+    }
+
+    public void TomarDano (int dano)
+    {
+        statusJogador.Vida -= dano;
+        scriptControlaInterface.AtualizarSliderVidaJogador();
+        ControlaAudio.instancia.PlayOneShot(SomDeDano);
+        if(statusJogador.Vida <= 0)
+        {
+            Morrer();
+        }        
+    }
+
+    public void Morrer ()
+    {
+        scriptControlaInterface.GameOver();
+    }
+}
---
+++ Unity parte 3/apocalipse-zumbi-alura/Assets/Scripts/GeradorZumbis.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorZumbis : MonoBehaviour {
+
+    public GameObject Zumbi;
+    private float contadorTempo = 0;
+    public float TempoGerarZumbi = 1;
+    public LayerMask LayerZumbi;
+    private float distanciaDeGeracao = 3;
+    private float DistanciaDoJogadorParaGeracao = 20;
+    private GameObject jogador;
+
+    private void Start()
+    {
+        jogador = GameObject.FindWithTag("Jogador");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if(Vector3.Distance(transform.position, 
+            jogador.transform.position) > 
+            DistanciaDoJogadorParaGeracao )
+        {
+            contadorTempo += Time.deltaTime;
+
+            if (contadorTempo >= TempoGerarZumbi)
+            {
+                StartCoroutine(GerarNovoZumbi());
+                contadorTempo = 0;
+            }
+        }        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, distanciaDeGeracao);
+    }
+
+    IEnumerator GerarNovoZumbi ()
+    {
+        Vector3 posicaoDeCriacao = AleatorizarPosicao();
+        Collider[] colisores = Physics.OverlapSphere(posicaoDeCriacao, 1, LayerZumbi);
+
+        while(colisores.Length > 0)
+        {
+            posicaoDeCriacao = AleatorizarPosicao();
+            colisores = Physics.OverlapSphere(posicaoDeCriacao, 1, LayerZumbi);
+            yield return null;
+        }
+
+        Instantiate(Zumbi, posicaoDeCriacao, transform.rotation);
+    }
+
+    Vector3 AleatorizarPosicao ()
+    {
+        Vector3 posicao = Random.insideUnitSphere * distanciaDeGeracao;
+        posicao += transform.position;
+        posicao.y = 0;
+
+        return posicao;
+    }
+}
