d639079848cd580eb0e93341e7b4773a4b0a9935
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-05-01 09:32:17 -0300

    Start Unity parte 3: Criando um chefão e refinando o jogo

---
+++ Unity parte 3/README.md
@@ -0,0 +1,3 @@
+# Unity parte 3: Criando um chefão e refinando o jogo
+
+## Aula 01 - Kit Médico
---
+++ Unity parte 3/apocalipse-zumbi-alura/Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour, IMatavel
+{
+
+    public GameObject Jogador;
+    private MovimentoPersonagem movimentaInimigo;
+    private AnimacaoPersonagem animacaoInimigo;
+    private Status statusInimigo;
+    public AudioClip SomDeMorte;
+    private Vector3 posicaoAleatoria;
+    private Vector3 direcao;
+    private float contadorVagar;
+    private float tempoEntrePosicoesAleatorias = 4;
+
+	// Use this for initialization
+	void Start () {
+        Jogador = GameObject.FindWithTag("Jogador");
+        animacaoInimigo = GetComponent<AnimacaoPersonagem>();
+        movimentaInimigo = GetComponent<MovimentoPersonagem>();
+        AleatorizarZumbi();
+        statusInimigo = GetComponent<Status>();
+    }
+
+    void FixedUpdate()
+    {
+        float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
+
+        movimentaInimigo.Rotacionar(direcao);
+        animacaoInimigo.Movimentar(direcao.magnitude);
+
+        if(distancia > 15)
+        {
+            Vagar ();
+        }
+        else if (distancia > 2.5)
+        {
+            direcao = Jogador.transform.position - transform.position;
+
+            movimentaInimigo.Movimentar(direcao, statusInimigo.Velocidade);
+
+            animacaoInimigo.Atacar(false);
+        }
+        else
+        {
+            direcao = Jogador.transform.position - transform.position;
+
+            animacaoInimigo.Atacar(true);
+        }
+    }
+
+    void Vagar ()
+    {
+        contadorVagar -= Time.deltaTime;
+        if(contadorVagar <= 0)
+        {
+            posicaoAleatoria = AleatorizarPosicao();
+            contadorVagar += tempoEntrePosicoesAleatorias;
+        }
+
+        bool ficouPertoOSuficiente = Vector3.Distance(transform.position, posicaoAleatoria) <= 0.05;
+        if (ficouPertoOSuficiente == false)
+        {
+            direcao = posicaoAleatoria - transform.position;
+            movimentaInimigo.Movimentar(direcao, statusInimigo.Velocidade);
+        }           
+    }
+
+    Vector3 AleatorizarPosicao ()
+    {
+        Vector3 posicao = Random.insideUnitSphere * 10;
+        posicao += transform.position;
+        posicao.y = transform.position.y;
+
+        return posicao;
+    }
+
+    void AtacaJogador ()
+    {
+        int dano = Random.Range(20, 30);
+        Jogador.GetComponent<ControlaJogador>().TomarDano(dano);
+    }
+
+    void AleatorizarZumbi ()
+    {
+        int geraTipoZumbi = Random.Range(1, 28);
+        transform.GetChild(geraTipoZumbi).gameObject.SetActive(true);
+    }
+
+    public void TomarDano(int dano)
+    {
+        statusInimigo.Vida -= dano;
+        if(statusInimigo.Vida <= 0)
+        {
+            Morrer();
+        }
+    }
+
+    public void Morrer()
+    {
+        Destroy(gameObject);
+        ControlaAudio.instancia.PlayOneShot(SomDeMorte);
+    }
+}
