b4a458f6cb228d18c8cbd47d42aaee194df816e1
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-05-04 20:03:23 -0300

    (Unity parte 3: Criando um chefão e refinando o jogo) Chefe de Fase: Nav Mesh Agent

---
+++ Unity parte 3/apocalipse-zumbi-alura/Assets/Scripts/ControlaChefe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ControlaChefe : MonoBehaviour
+{
+    private Transform jogador;
+    private NavMeshAgent agente;
+
+    private void Start()
+    {
+        jogador = GameObject.FindWithTag("Jogador").transform;
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agente.SetDestination(jogador.position);
+    }
+}
