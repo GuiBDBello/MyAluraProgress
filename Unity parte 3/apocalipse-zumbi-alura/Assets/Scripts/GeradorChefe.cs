1fc9a35dd18adae7065893e93d015b1bef60b2c1
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-05-04 21:40:17 -0300

    (Unity parte 3: Criando um chefão e refinando o jogo) Retoques Finais: Gerador do Chefe

---
+++ Unity parte 3/apocalipse-zumbi-alura/Assets/Scripts/GeradorChefe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorChefe : MonoBehaviour
+{
+    public GameObject ChefePrefab;
+
+    private float tempoParaProximaGeracao = 0;
+    public float tempoEntreGeracoes = 30;
+
+    private void Start()
+    {
+        tempoParaProximaGeracao = tempoEntreGeracoes;
+    }
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad > tempoParaProximaGeracao)
+        {
+            Instantiate(ChefePrefab, transform.position, Quaternion.identity);
+            tempoParaProximaGeracao = Time.timeSinceLevelLoad + tempoEntreGeracoes;
+        }
+    }
+}
