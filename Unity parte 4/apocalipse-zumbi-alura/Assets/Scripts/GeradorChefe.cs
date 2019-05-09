8120fd3dcf6f83a3f2ceb21b3e8a485767904606
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-05-08 22:35:40 -0300

    (Unity parte 4: Chefão, partículas e menu) Retoques no Chefe do Jogo: Modificando o Gerador

--- Unity parte 4/apocalipse-zumbi-alura/Assets/Scripts/GeradorChefe.cs
+++ Unity parte 4/apocalipse-zumbi-alura/Assets/Scripts/GeradorChefe.cs
@@ -6,23 +6,45 @@ public class GeradorChefe : MonoBehaviour
 {
     public float tempoEntreGeracoes = 60;
     public GameObject ChefePrefab;
+    public Transform[] PosicoesPossiveisDeGeracao;
 
     private float tempoParaProximaGeracao = 0;
     private ControlaInterface scriptControlaInterface;
+    private Transform jogador;
 
     private void Start()
     {
         tempoParaProximaGeracao = tempoEntreGeracoes;
         scriptControlaInterface = GameObject.FindObjectOfType(typeof(ControlaInterface)) as ControlaInterface;
+        jogador = GameObject.FindWithTag("Jogador").transform;
     }
 
     private void Update()
     {
         if (Time.timeSinceLevelLoad > tempoParaProximaGeracao)
         {
-            Instantiate(ChefePrefab, transform.position, Quaternion.identity);
+            Vector3 posicaoDeCriacao = CalcularPosicaoMaisDistanteDoJogador();
+            Instantiate(ChefePrefab, posicaoDeCriacao, Quaternion.identity);
             scriptControlaInterface.AparecerTextoChefeCriado();
             tempoParaProximaGeracao = Time.timeSinceLevelLoad + tempoEntreGeracoes;
         }
+    }
+
+    Vector3 CalcularPosicaoMaisDistanteDoJogador()
+    {
+        Vector3 posicaoDeMaiorDistancia = Vector3.zero;
+        float maiorDistancia = 0;
+
+        foreach(Transform posicao in PosicoesPossiveisDeGeracao)
+        {
+            float distanciaEntreOJogador = Vector3.Distance(posicao.position, jogador.position);
+            if (distanciaEntreOJogador > maiorDistancia)
+            {
+                maiorDistancia = distanciaEntreOJogador;
+                posicaoDeMaiorDistancia = posicao.position;
+            }
+        }
+
+        return posicaoDeMaiorDistancia;
     }
 }
