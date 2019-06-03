ade9b99df8cdfa7a3357eb969bcbd8609a435d21
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-03 20:10:43 -0300

    (Java parte 5: Pacotes e java.lang) Todos os modificadores de acesso: Revistando modificadores de acesso

--- Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
+++ Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
@@ -1,11 +1,16 @@
 package br.com.bytebank.banco.teste;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+import br.com.bytebank.banco.especial.ContaEspecial;
 import br.com.bytebank.banco.modelo.*;
 
 public class TesteContas {
 
 	public static void main(String[] args) throws SaldoInsuficienteException{
-		//Full Qualified Name FQN
+		
+		ContaEspecial ce = new ContaEspecial(123, 5555);
+		
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
 		
