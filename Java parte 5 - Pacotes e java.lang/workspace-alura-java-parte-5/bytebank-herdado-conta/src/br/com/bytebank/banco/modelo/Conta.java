3c3275d89f75594c8953632cdb0e0f38d7b71ce5
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-03 21:05:11 -0300

    (Java parte 5: Pacotes e java.lang) Distribuição do seu código: Conhecendo o Javadoc

--- Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,10 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe representa a moldura de uma conta.
+ * 
+ * @author GuiDB
+ */
 public abstract class Conta {
 
     double saldo;
@@ -8,6 +13,12 @@ public abstract class Conta {
     private Cliente titular;
     private static int total = 0;
     
+    /**
+     * Construtor para inicializar o objeto Conta a partir da agência e número.
+     * 
+     * @param agencia
+     * @param numero
+     */
     public Conta(int agencia, int numero){
         Conta.total++;
         //System.out.println("O total de contas é " + Conta.total);
@@ -19,6 +30,12 @@ public abstract class Conta {
 
     public abstract void deposita(double valor);
 
+    /**
+     * Valor precisa ser maior do que o saldo.
+     * 
+     * @param valor
+     * @throws SaldoInsuficienteException
+     */
     public void saca(double valor) throws SaldoInsuficienteException{
     	
         if(this.saldo < valor) {
