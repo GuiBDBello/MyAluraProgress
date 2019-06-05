551c74f26237a021ad10b88c22e680b03b0de8f1
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-04 21:38:42 -0300

    (Java parte 5: Pacotes e java.lang) O pacote java.lang: Metodos da classe String

--- Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -3,17 +3,44 @@ package br.com.bytebank.banco.teste;
 public class TesteString {
 	
 	public static void main(String[] args) {
+		
+		String vazio = " Alura ";
+		String outroVazio = vazio.trim();
+		
+		System.out.println(vazio.isEmpty());
+		System.out.println(outroVazio.isEmpty());
+		
+		System.out.println(vazio);
+		System.out.println(outroVazio.contains("Alu"));
 
-		int a = 3;
 		String nome = "Alura";//object literal
-		//String outro = new String("Alura");
+//		String outro = new String("Alura");
 		
-		//String outra = nome.replace("A", "a");
+//		System.out.println(nome.length());
+//		
+//		for (int i = 0; i < nome.length(); i++) {
+//			System.out.println(nome.charAt(i));
+//		}
 		
-		String outra = nome.toLowerCase();
+//		String sub = nome.substring(1);
+//		System.out.println(sub);
 		
-		System.out.println(nome);
-		System.out.println(outra);
+//		int pos = nome.indexOf("ur");
+//		System.out.println(pos);
+		
+//		char c = nome.charAt(0);
+//		System.out.println(c);
+		
+//		char c = 'A';
+//		char d = 'a';
+//		String outra = nome.replace(c, d);
+//		String outra = nome.replace("Al", "aL");
+		
+//		String outra = nome.toLowerCase();
+//		String outra = nome.toUpperCase();
+		
+//		System.out.println(nome);
+//		System.out.println(outra);
 		
 	}
 
