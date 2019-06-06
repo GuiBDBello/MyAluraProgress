27ded7867e67c220ac6294def247707a823ebbc9
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-05 23:28:12 -0300

    (Java parte 5: Pacotes e java.lang) A classe Object: O método toString()

--- Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -9,9 +9,9 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(false);
+		//		System.out.println("x");
+		//		System.out.println(3);
+		//		System.out.println(false);
 		
 		ContaCorrente cc = new ContaCorrente(22, 33);
 		Object cp = new ContaPoupanca(33, 22);
@@ -19,8 +19,8 @@ public class Teste {
 		
 		System.out.println(cc.toString());
 		System.out.println(cp);
-		
-//		println(cc);
+
+		//		println(cc);
 		
 	}
 	
