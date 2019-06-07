ce7375a84b3d7e017eec3d340184915a0a873c21
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-07 19:27:25 -0300

    (Java parte 6: Conhecendo o java.util) Conhecendo Arrays: Começando com Arrays

--- Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ Java parte 5 - Pacotes e java.lang/workspace-alura-java-parte-5/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -1,33 +1,32 @@
 package br.com.bytebank.banco.teste;
 
-import br.com.bytebank.banco.modelo.Cliente;
-import br.com.bytebank.banco.modelo.Conta;
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
-
 public class Teste {
 
+	// Array []
 	public static void main(String[] args) {
 		
-		//		System.out.println("x");
-		//		System.out.println(3);
-		//		System.out.println(false);
+		int[] idades = new int[5];//inicializa o array com 0
 		
-		ContaCorrente cc = new ContaCorrente(22, 33);
-		Object cp = new ContaPoupanca(33, 22);
-		Object cliente = new Cliente();
+		for (int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+		}
 		
-		System.out.println(cc.toString());
-		System.out.println(cp);
-
-		//		println(cc);
+		for (int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+		
+//		idades[0] = 29;
+//		idades[1] = 39;
+//		idades[2] = 49;
+//		idades[3] = 59;
+//		idades[4] = 69;
+//		
+//		int idade1 = idades[4];
+//		
+//		System.out.println(idade1);
+//		
+//		System.out.println(idades.length);
 		
 	}
-	
-	static void println() {}
-	static void println(int a) {}
-	static void println(boolean valor) {}
-	static void println(Conta conta) {}
-	static void println(Object referencia) {}
 	
 }
--- Java parte 6 - Conhecendo o java.util/README.md
+++ Java parte 6 - Conhecendo o java.util/README.md
@@ -1,1 +1,15 @@
 # Java parte 6: Conhecendo o java.util
+
+## Aula 01 - Conhecendo Arrays
+
+### Atividade 02 - Começando com Arrays:
+
+- Array é um Objeto, portanto, é necessário inicializá-lo utilizando a palavra `new`;
+- **Ex.:** `int[] idades = new int[5];`
+- O Array é inicializado com valores padrão, como uma Classe (int = 0, String = null);
+- Para acessar uma posição do Array, utiliza-se colchetes e o número da posição;
+- **Ex.:** `int idade1 = idades[0];`
+- Ao tentar acessar uma posição que não existe, ocorre um `ArrayIndexOutOfBoundsException`;
+- `idades.length;`: atributo de um Objeto do tipo Array que retorna o tamanho daquele Array;
+- É possível fazer um laço com base num Array, utilizando-o como critério de parada;
+- **Ex.:** `for(int i = 0; i < idades.length; i++) { }`
