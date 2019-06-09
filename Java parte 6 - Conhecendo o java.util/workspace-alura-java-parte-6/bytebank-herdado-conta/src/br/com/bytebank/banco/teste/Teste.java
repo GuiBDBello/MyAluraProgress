2fbe149d8217fc4ff3a23ab9a8722bfe8662ad02
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-08 21:14:31 -0300

    (Java parte 6: Conhecendo o java.util) ArrayList e Generics: Mão na massa: GuardadorDeContas - Desafio

--- Java parte 6 - Conhecendo o java.util/workspace-alura-java-parte-6/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ Java parte 6 - Conhecendo o java.util/workspace-alura-java-parte-6/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -2,13 +2,13 @@ package br.com.bytebank.banco.teste;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.GuardadorDeContas;
+import br.com.bytebank.banco.modelo.GuardadorDeObjetos;
 
 public class Teste {
 	
 	public static void main(String[] args) {
 		
-		GuardadorDeContas guardador = new GuardadorDeContas();
+		GuardadorDeObjetos guardador = new GuardadorDeObjetos();
 		
 		Conta cc = new ContaCorrente(22, 11);
 		guardador.adiciona(cc);
@@ -19,7 +19,7 @@ public class Teste {
 		int tamanho = guardador.getQuantidadeDeElementos();
 		System.out.println(tamanho);
 		
-		Conta ref = guardador.getReferencia(1);
+		Conta ref = (Conta) guardador.getReferencia(1);
 		System.out.println(ref.getNumero());
 		
 	}
