527c26c3e7f917c866b02d933c012d3f1fa2d822
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-20 09:25:16 -0300

    (Java parte 7: Trabalhando com java.io) Usando java.util.Scanner: Formatação de valores

--- Java parte 7 - Trabalhando com java.io/workspace-alura-java-parte-7/java-io/src/br/com/alura/java/io/test/TesteLeitura2.java
+++ Java parte 7 - Trabalhando com java.io/workspace-alura-java-parte-7/java-io/src/br/com/alura/java/io/test/TesteLeitura2.java
@@ -14,19 +14,21 @@ public class TesteLeitura2 {
 		
 		while(scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
-			System.out.println(linha);
+//			System.out.println(linha);
 			
 			Scanner linhaScanner = new Scanner(linha);
 			linhaScanner.useLocale(Locale.US);
 			linhaScanner.useDelimiter(",");
 			
-			String valor1 = linhaScanner.next();
-			int valor2 = linhaScanner.nextInt();
-			int valor3 = linhaScanner.nextInt();
-			String valor4 = linhaScanner.next();
-			double valor5 = linhaScanner.nextDouble();
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
 			
-			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+			System.out.format(String.format(new Locale("pt", "BR"), 
+					"%s - %04d-%08d, %20s: %08.2f %n",
+					tipoConta, agencia, numero, titular, saldo));
 			
 			linhaScanner.close();
 			
