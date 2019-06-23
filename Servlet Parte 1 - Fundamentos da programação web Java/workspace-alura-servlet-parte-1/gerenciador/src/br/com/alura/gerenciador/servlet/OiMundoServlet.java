2e8b4bd90e80e88cf0d7e8278afad9956b128d81
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-06-23 11:57:55 -0300

    (Servlet Parte 1: Fundamentos da programação web Java) Fundamentos da Web e a API de Servlets: Finalmente, o primeiro servlet

---
+++ Servlet Parte 1 - Fundamentos da programação web Java/workspace-alura-servlet-parte-1/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,30 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi mundo! Parabéns, você escreveu seu primeiro Servlet!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O Servlet 'OiMundoServlet' foi chamado");
+		
+	}
+	
+}
