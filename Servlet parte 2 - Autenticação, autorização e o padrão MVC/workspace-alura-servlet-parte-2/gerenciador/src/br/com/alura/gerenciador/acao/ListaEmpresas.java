24b18e3208ab644d489aea624cc5b618f402e0e4
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-07-13 18:21:51 -0300

    (Servlet parte 2: Autenticação, autorização e o padrão MVC) HttpSession: Testando o login

--- Servlet parte 2 - Autenticação, autorização e o padrão MVC/workspace-alura-servlet-parte-2/gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ Servlet parte 2 - Autenticação, autorização e o padrão MVC/workspace-alura-servlet-parte-2/gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -3,10 +3,10 @@ package br.com.alura.gerenciador.acao;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -15,6 +15,11 @@ public class ListaEmpresas implements Acao {
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		HttpSession sessao = request.getSession();
+		if (sessao.getAttribute("usuarioLogado") == null) {
+			return "redirect:entrada?acao=LoginForm";
+		}
+		
 		System.out.println("Listando empresas.");
 		
 		Banco banco = new Banco();
