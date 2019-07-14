f5c5d98fc193e25c3f652dc0eaf2026e6820a742
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-07-14 17:38:42 -0300

    (Servlet parte 2: Autenticação, autorização e o padrão MVC) Aplicando filtros: Ordem de execução

--- Servlet parte 2 - Autenticação, autorização e o padrão MVC/workspace-alura-servlet-parte-2/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ Servlet parte 2 - Autenticação, autorização e o padrão MVC/workspace-alura-servlet-parte-2/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -21,32 +21,32 @@ import br.com.alura.gerenciador.acao.RemoveEmpresa;
 /**
  * Servlet implementation class UnicaEntradaServlet
  */
-@WebServlet("/entrada")
+//@WebServlet("/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String paramAcao = request.getParameter("acao");
-		
-		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
-		
-		String nome;
-		try {
-			Class classe = Class.forName(nomeDaClasse); // Carrega a Classe com o nome 'nomeDaClasse'
-			Acao acao = (Acao) classe.newInstance();
-			nome = acao.executa(request, response);
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-			throw new ServletException(e);
-		}
-		
-		String[] tipoEEndereco = nome.split(":");
-		if (tipoEEndereco[0].equals("forward")) {
-			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]);
-			rd.forward(request, response);
-		} else {
-			response.sendRedirect(tipoEEndereco[1]);
-		}
+//		String paramAcao = request.getParameter("acao");
+//		
+//		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
+//		
+//		String nome;
+//		try {
+//			Class classe = Class.forName(nomeDaClasse); // Carrega a Classe com o nome 'nomeDaClasse'
+//			Acao acao = (Acao) classe.newInstance();
+//			nome = acao.executa(request, response);
+//		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+//			throw new ServletException(e);
+//		}
+//		
+//		String[] tipoEEndereco = nome.split(":");
+//		if (tipoEEndereco[0].equals("forward")) {
+//			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]);
+//			rd.forward(request, response);
+//		} else {
+//			response.sendRedirect(tipoEEndereco[1]);
+//		}
 		
 //		paramAcao.executa(request, response);
 		
