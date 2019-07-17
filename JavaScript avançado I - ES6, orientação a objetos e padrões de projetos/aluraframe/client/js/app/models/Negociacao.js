48c5b4a7e1fe44da1bcac8b96abab12341afe539
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-07-17 20:17:03 -0300

    (JavaScript avançado I: ES6, orientação a objetos e padrões de projetos) Especificando uma Negociação: Encapsulamento

--- JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/models/Negociacao.js
+++ JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/models/Negociacao.js
@@ -1,13 +1,25 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
-    obtemVolume() {
-        return this.quantidade * this.valor;
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
     }
 
 }
