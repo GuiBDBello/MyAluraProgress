e3ba746a8de7e50e79053eb958f5b5ddca08fcfd
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-07-18 19:56:44 -0300

    (JavaScript avançado I: ES6, orientação a objetos e padrões de projetos) A ligação entre as ações do usuário e o modelo: Associando métodos do controller às ações do usuário

--- JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,7 +2,15 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        alert('Chamei a ação no controller!');
+
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 
 }
