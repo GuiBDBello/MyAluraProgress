d5622f387a63c047937061a681f95356f44ad1e9
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-07-19 20:11:29 -0300

    (JavaScript avançado I: ES6, orientação a objetos e padrões de projetos) A ligação entre as ações do usuário e o modelo: Criando uma instância de Negociacao

--- JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,17 +2,26 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
     adiciona(event) {
         event.preventDefault();
 
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        console.log(typeof(this._inputData.value));
+
+        console.log(this._inputData.value);
+
+        // let negociacao = new Negociacao(
+        //     this._inputData.value,
+        //     this._inputQuantidade.value,
+        //     this._inputValor.value
+        // );
+        // console.log(negociacao);
+
+        // Adicionar a negociação em uma lista
     }
 
 }
