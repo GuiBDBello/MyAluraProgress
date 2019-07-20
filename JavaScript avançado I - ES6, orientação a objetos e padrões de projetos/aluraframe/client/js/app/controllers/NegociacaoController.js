ff8a8afd833070210b859a7d8f094ca1ab54e121
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-07-20 11:57:48 -0300

    (JavaScript avançado I: ES6, orientação a objetos e padrões de projetos) A ligação entre as ações do usuário e o modelo: Arrow Functions: deixando o código ainda menos verboso

--- JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -13,12 +13,16 @@ class NegociacaoController {
         let data = new Date(...
             this._inputData.value
                 .split('-')
-                .map(function(item, indice) {
-                    return item - indice % 2;
-                })
+                .map((item, indice) => item - indice % 2)
+        );
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
         );
 
-        console.log(data);
+        console.log(negociacao);
     }
 
 }
