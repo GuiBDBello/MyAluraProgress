868c56ac92d4d7ab673fd66d1cd3eb8826bf48c7
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-07-23 22:47:39 -0300

    (JavaScript avançado I: ES6, orientação a objetos e padrões de projetos) Generalizando a solução da nossa View: Construtor vs super

--- JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/views/View.js
+++ JavaScript avançado I - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/views/View.js
@@ -4,7 +4,11 @@ class View {
         this._elemento = elemento;
     }
 
+    template() {
+        throw new Error('O método template deve ser implementado.');
+    }
+
     update(model) {
-        this._elemento.innerHTML = this._template(model);
+        this._elemento.innerHTML = this.template(model);
     }
 }
