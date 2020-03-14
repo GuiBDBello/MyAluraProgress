8121a707f6f1161445d480c0701546c2d14e018b
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-03-13 22:14:53 -0300

    (JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos) E se alguém criasse nossos proxies? O Padrão de Projeto Factory: Consolidando seu conhecimento 2 (importante)

--- JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/services/ProxyFactory.js
+++ JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/services/ProxyFactory.js
@@ -6,18 +6,18 @@ class ProxyFactory {
                 if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
                     return function() {
                         console.log(`Interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        return acao(target);
+                        let retorno = Reflect.apply(target[prop], target, arguments);
+                        acao(target);
+                        return retorno;
                     }
                 }
                 return Reflect.get(target, prop, receiver);
             },
             set(target, prop, value, receiver) {
-                if (props.includes(prop)) {
-                    target[prop] = value;
-                    acao(target);
-                }
-                return Reflect.set(target, prop, value, receiver);
+
+                let retorno = Reflect.set(target, prop, value, receiver);
+                if (props.includes(prop)) acao(target);
+                return retorno;
             }
         });
     }
