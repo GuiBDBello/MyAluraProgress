609d43408e8b2cd95af8a5dc4d75d190c3349fb6
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-03-15 20:21:58 -0300

    (JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos) Considerações finais e exercícios bônus: Ah se meu código funcionasse no Firefox...

--- JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/helpers/DateHelper.js
+++ JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/helpers/DateHelper.js
@@ -7,13 +7,15 @@ class DateHelper {
     static dataParaTexto(data) {
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
-
+    
     static textoParaData(texto) {
-        
-        if (!/\d{4}-\d{2}-\d{2}/.test(texto))
-            throw new Error('Deve estar no formato aaaa-mm-dd');
 
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+        // mudamos a validação para aceitar o novo formato!
+        if(!/\d{2}\/\d{2}\/\d{4}/.test(texto)) 
+            throw new Error('Deve estar no formato dd/mm/aaaa');
+
+        // veja que usamos no split '/' no lugar de '-'. Usamos `reverse` também para ficar ano/mes/dia.      
+        return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
     }
 
 }
