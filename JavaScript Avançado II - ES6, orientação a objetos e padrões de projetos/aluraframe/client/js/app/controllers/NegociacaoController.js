8ab6a4002092ff660a1265ed613affb62c6f9d16
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-03-15 18:36:04 -0300

    (JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos) Combatendo Callback Hell com Promises: Desafio: obter todas as negociações

--- JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -31,18 +31,13 @@ class NegociacaoController {
     importaNegociacoes() {
         
         let service = new NegociacaoService();
-
-        Promise.all([
-            service.obterNegociacoesDaSemana(),
-            service.obterNegociacoesDaSemanaAnterior(),
-            service.obterNegociacoesDaSemanaRetrasada()
-        ]).then(negociacoes => {
-            negociacoes
-                .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
-                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-            this._mensagem.texto = 'Negociações importadas com sucesso.';
-        }).catch(erro => this._mensagem.texto = erro);
-
+        service
+            .obterNegociacoes()
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações do período importadas com sucesso';
+            })
+            .catch(error => this._mensagem.texto = error);
     }
 
     apaga() {
