7de118629f54b50fa0057cdb9233a06a2b8ba602
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-03-15 20:09:15 -0300

    (JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos) Considerações finais e exercícios bônus: Ah se meu HttpService fizesse mais coisa...

--- JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/services/HttpService.js
+++ JavaScript Avançado II - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/services/HttpService.js
@@ -1,5 +1,6 @@
 class HttpService {
     get(url) {
+
         return new Promise((resolve, reject) => {
 
             let xhr = new XMLHttpRequest();
@@ -29,5 +30,31 @@ class HttpService {
 
             xhr.send();
         });
+    }
+
+    post(url, dado) {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onreadystatechange = () => {
+
+                if (xhr.readyState == 4) {
+
+                    if (xhr.status == 200) {
+
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+            // usando JSON.stringify para converter objeto em uma string no formato JSON.
+            xhr.send(JSON.stringify(dado));
+        });
+
     }
 }
