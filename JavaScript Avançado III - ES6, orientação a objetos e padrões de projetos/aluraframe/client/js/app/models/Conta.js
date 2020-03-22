5335f63355ace84b81e75a23be457e52a99b987f
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-03-22 20:30:06 -0300

    (JavaScript Avançado III: ES6, orientação a objetos e padrões de projetos) Tornando nosso código ainda mais compatível usando Babel: Executando o Babel

---
+++ JavaScript Avançado III - ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/models/Conta.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Conta = function () {
+    function Conta() {
+        var saldo = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+
+        _classCallCheck(this, Conta);
+
+        this._saldo = saldo;
+    }
+
+    _createClass(Conta, [{
+        key: 'atualiza',
+        value: function atualiza(taxa) {
+            throw new Error('O método atualiza deve ser implementado.');
+        }
+    }, {
+        key: 'saldo',
+        get: function get() {
+            return this._saldo;
+        }
+    }]);
+
+    return Conta;
+}();
+//# sourceMappingURL=Conta.js.map
