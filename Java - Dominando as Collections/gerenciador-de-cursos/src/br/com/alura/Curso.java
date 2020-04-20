698a93e02ab03485eb9a4e88a3cc59e460431782
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-04-19 21:51:43 -0300

    (Java: Dominando as Collections) Aplicando o Set no modelo: Aplicando o Set no modelo

--- Java - Dominando as Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ Java - Dominando as Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -2,13 +2,16 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<Aula>();
+	private Set<Aluno> alunos = new HashSet<Aluno>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -43,5 +46,13 @@ public class Curso {
 	@Override
 	public String toString() {
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", " + this.aulas + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
 	}
 }
