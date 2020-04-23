fcdd77d79e1db257793be81bbe14c8766cd9b0cc
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-04-22 23:12:35 -0300

    (Java: Dominando as Collections) Mapas: Mapas

--- Java - Dominando as Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ Java - Dominando as Collections/gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -2,9 +2,13 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -14,6 +18,7 @@ public class Curso {
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<Aula>();
 	private Set<Aluno> alunos = new HashSet<Aluno>();
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -31,20 +36,20 @@ public class Curso {
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
 	}
-	
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
-	
+
 	public int getTempoTotal() {
-//		int tempoTotal = 0;
-//		for (Aula aula : aulas) {
-//			tempoTotal += aula.getTempo();
-//		}
-//		return tempoTotal;
+		// int tempoTotal = 0;
+		// for (Aula aula : aulas) {
+		// tempoTotal += aula.getTempo();
+		// }
+		// return tempoTotal;
 		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
 	}
-	
+
 	@Override
 	public String toString() {
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", " + this.aulas + "]";
@@ -52,13 +57,25 @@ public class Curso {
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
 	}
-	
+
 	public Set<Aluno> getAlunos() {
 		return Collections.unmodifiableSet(alunos);
 	}
 
 	public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
+	}
+
+	public Aluno buscaMatriculado(int numero) {
+//		for (Aluno aluno : alunos) {
+//			if (aluno.getNumeroMatricula() == numero)
+//				return aluno;
+//		}
+//		throw new NoSuchElementException("Matrícula não encontrada: " + numero);
+		if (!matriculaParaAluno.containsKey(numero))
+			throw new NoSuchElementException();
+		return matriculaParaAluno.get(numero);
 	}
 }
