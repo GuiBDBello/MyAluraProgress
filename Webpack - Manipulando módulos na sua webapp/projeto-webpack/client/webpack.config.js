abaddf66cbbe13221ca7b930ca26311047f9a25c
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-06-30 23:08:38 -0300

    (Webpack: Manipulando módulos na sua webapp) Boas práticas: Separando o nosso código das bibliotecas

--- Webpack - Manipulando módulos na sua webapp/projeto-webpack/client/webpack.config.js
+++ Webpack - Manipulando módulos na sua webapp/projeto-webpack/client/webpack.config.js
@@ -13,9 +13,14 @@ plugins.push(new webpack.ProvidePlugin({
     'jQuery': 'jquery/dist/jquery.js'
 }));
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}));
+
 if (process.env.NODE_ENV == 'production') {
 
-    plugins.push(webpack.optimize.ModuleConcatenationPlugin());
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
@@ -30,7 +35,10 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
