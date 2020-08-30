b2365ad4f9ac852bf4f849dbbb04d78f35c2507a
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-08-30 15:16:30 -0300

    (Testes: TDD com Java) Começando com testes de unidade: Começando com testes de unidade

---
+++ Testes - TDD com Java/workspace/teste-de-unidade/src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,37 @@
+package br.com.caelum.leilao.teste;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.servico.Avaliador;
+import junit.framework.Assert;
+
+public class TesteDoAvaliador {
+	
+	@Test
+	public void deveEntenderLancesEmOrdemCrescente() {
+		// parte 1: cenario
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+		
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(jose, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+		
+		// parte 2: acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		// parte 3: validação
+		double maiorEsperado = 400;
+		double menorEsperado = 250;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+	}
+}
