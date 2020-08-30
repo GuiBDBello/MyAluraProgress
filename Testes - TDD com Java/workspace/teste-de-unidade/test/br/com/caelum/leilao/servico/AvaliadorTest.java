44e64e0c3ed81524c5015fcb1d5f332ad94f01d4
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-08-30 16:17:48 -0300

    (Testes: TDD com Java) Começando com testes de unidade: Média dos Lances

--- Testes - TDD com Java/workspace/teste-de-unidade/test/br/com/caelum/leilao/servico/AvaliadorTest.java
+++ Testes - TDD com Java/workspace/teste-de-unidade/test/br/com/caelum/leilao/servico/AvaliadorTest.java
@@ -34,4 +34,42 @@ public class AvaliadorTest {
 		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
 		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
 	}
+	
+	@Test
+    public void deveCalcularAMedia() {
+        // cenario: 3 lances em ordem crescente
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(maria,300.0));
+        leilao.propoe(new Lance(joao,400.0));
+        leilao.propoe(new Lance(jose,500.0));
+
+        // executando a acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        // comparando a saida com o esperado
+        Assert.assertEquals(400, leiloeiro.getMedia(), 0.0001);
+    }
+
+    @Test
+    public void testaMediaDeZeroLance(){
+
+        // cenario
+        Usuario ewertom = new Usuario("Ewertom");
+
+        // acao
+        Leilao leilao = new Leilao("Iphone 7");
+
+        Avaliador avaliador = new Avaliador();
+        avaliador.avalia(leilao);
+
+        //validacao
+        Assert.assertEquals(0, avaliador.getMedia(), 0.0001);
+
+    }
 }
