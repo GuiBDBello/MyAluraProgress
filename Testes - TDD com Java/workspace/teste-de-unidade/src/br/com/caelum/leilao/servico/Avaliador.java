7557945e28d5442aa47df7dc76eb7d03f4b57597
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-08-30 17:42:25 -0300

    (Testes: TDD com Java) Testando o que realmente é necessário: Três maiores lances

--- Testes - TDD com Java/workspace/teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
+++ Testes - TDD com Java/workspace/teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
@@ -10,37 +10,40 @@ import br.com.caelum.leilao.dominio.Leilao;
 
 public class Avaliador {
 
-	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
-	private double menorDeTodos = Double.POSITIVE_INFINITY;
-	private double media = 0;
-	private List<Lance> maiores;
+    private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+    private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private List<Lance> maiores;
 
-	public void avalia(Leilao leilao) {
-		double total = 0;
-		for(Lance lance : leilao.getLances()) {
-			if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
-			if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
-			total += lance.getValor();
-		}
-		if(total == 0) {
-			media = 0;
-			return;
-		}
-		media = total / leilao.getLances().size();
-		
-		maiores = new ArrayList<Lance>(leilao.getLances());
-		Collections.sort(maiores, new Comparator<Lance>() {
-			public int compare(Lance o1, Lance o2) {
-				if(o1.getValor() < o2.getValor()) return 1;
-				if(o1.getValor() > o2.getValor()) return -1;
-				return 0;
-			}
-		});
-		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
-	}
-	
-	public List<Lance> getTresMaiores() { return maiores; }
-	public double getMaiorLance() { return maiorDeTodos; }
-	public double getMenorLance() { return menorDeTodos; }
-	public double getMedia() { return media; }
+    public void avalia(Leilao leilao) {
+        for(Lance lance : leilao.getLances()) {
+            if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
+            if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+        }
+
+        pegaOsMaioresNo(leilao);
+    }
+
+    private void pegaOsMaioresNo(Leilao leilao) {
+        maiores = new ArrayList<Lance>(leilao.getLances());
+        Collections.sort(maiores, new Comparator<Lance>() {
+            public int compare(Lance o1, Lance o2) {
+                if(o1.getValor() < o2.getValor()) return 1;
+                if(o1.getValor() > o2.getValor()) return -1;
+                return 0;
+            }
+        });
+        maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
+    }
+
+    public List<Lance> getTresMaiores() {
+        return this.maiores;
+    }
+
+    public double getMaiorLance() {
+        return maiorDeTodos;
+    }
+
+    public double getMenorLance() {
+        return menorDeTodos;
+    }
 }
