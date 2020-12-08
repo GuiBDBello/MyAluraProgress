c5f52a01ba772bdbda6ae45da0465ea497936060
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-12-08 07:15:40 -0300

    (Eclipse: Produtividade Extrema na IDE com Java) Mais flexibilidade ao gerar conteúdo: Mais flexibilidade ao gerar conteúdo - Forget exploded

--- Eclipse - Produtividade Extrema na IDE com Java/fj15-empresa/src/main/java/br/com/caelum/empresa/TesteImportador.java
+++ Eclipse - Produtividade Extrema na IDE com Java/fj15-empresa/src/main/java/br/com/caelum/empresa/TesteImportador.java
@@ -3,28 +3,27 @@ package br.com.caelum.empresa;
 import java.io.ByteArrayInputStream;
 import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
-import java.util.List;
+import java.util.Collection;
 
 import br.com.caelum.empresa.leitor.ImportadorDeGastos;
 import br.com.caelum.empresa.modelo.Gasto;
 
 public class TesteImportador {
 
-    public static void main(String[] args) throws UnsupportedEncodingException, ParseException {
-        String conteudo = 
-                "CARTAO01012011000010000123jbjasbd jbjbbb                22071983\r\n" +
-                "CARTAO01012011000010000123jbjasbd jbjbbb                22071983\r\n" +
-                "CARTAO01012011000010000123jbjasbd jbjbbb                22071983\r\n";
+	public static void main(String[] args) throws UnsupportedEncodingException,
+			ParseException {
+		String conteudo = 
+				"CARTAO01012011000010000123Jose da Silva                 22071983\r\n" +
+				"CARTAO01012011000010000123Jose da Silva                 22071983\r\n" +
+				"CARTAO01012011000010000123Jose da Silva                 22071983\r\n";
 
-        ImportadorDeGastos importador = new ImportadorDeGastos();
+		ImportadorDeGastos importador = new ImportadorDeGastos();
 
-        List<Gasto> list = importador.importa(new ByteArrayInputStream(conteudo.getBytes("UTF-8")));
+		Collection<Gasto> list = importador.importa(new ByteArrayInputStream(
+				conteudo.getBytes("UTF-8")));
 
-        for (Gasto gasto : list) {
-            System.out.println(gasto.getTipo());
-            System.out.println(gasto.getValor());
-            System.out.println(gasto.getFuncionario().getNome().trim());
-            System.out.println();
+		for (Gasto gasto : list) {
+            System.out.println(gasto);
         }
-    }
+	}
 }
