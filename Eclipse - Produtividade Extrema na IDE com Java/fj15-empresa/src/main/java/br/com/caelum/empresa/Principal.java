35e3694d9ab2bc76bc4bf4297c991912827a62c5
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-12-08 07:15:40 -0300

    (Eclipse: Produtividade Extrema na IDE com Java) Mais flexibilidade ao gerar conteúdo: Mais flexibilidade ao gerar conteúdo - Forget exploded

--- Eclipse - Produtividade Extrema na IDE com Java/fj15-empresa/src/main/java/br/com/caelum/empresa/Principal.java
+++ Eclipse - Produtividade Extrema na IDE com Java/fj15-empresa/src/main/java/br/com/caelum/empresa/Principal.java
@@ -6,9 +6,11 @@ import java.util.GregorianCalendar;
 import br.com.caelum.empresa.modelo.Funcionario;
 
 public class Principal {
+
 	public static void main(String[] args) {
 		Calendar dataNascimento = new GregorianCalendar(1985, 3, 18);
-		Funcionario funcionario = new Funcionario("José", 25, dataNascimento);
-		System.out.println(funcionario);
+	    Funcionario funcionario = new Funcionario("José", 25, dataNascimento);
+	    System.out.println(funcionario);
 	}
+
 }
