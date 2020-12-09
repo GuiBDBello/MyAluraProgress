c3078f02e3a3d3ee03033d0fdd042ece6a261406
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-12-08 23:15:56 -0300

    (Eclipse: Produtividade Extrema na IDE com Java) Escrevendo Menos Código com Templates: Escrevendo Menos Código com Templates

--- Eclipse - Produtividade Extrema na IDE com Java/fj15-empresa/src/main/java/br/com/caelum/empresa/dao/GastoDAO.java
+++ Eclipse - Produtividade Extrema na IDE com Java/fj15-empresa/src/main/java/br/com/caelum/empresa/dao/GastoDAO.java
@@ -1,12 +1,30 @@
 package br.com.caelum.empresa.dao;
 
+import java.util.List;
+
+//import org.apache.log4j.Logger;
+
 import br.com.caelum.empresa.modelo.Gasto;
 
-
-public class GastoDAO extends DAO<Gasto> {
-
-	public GastoDAO() {
-		super(Gasto.class);
+public class GastoDAO {
+	private DAO<Gasto> dao = new DAO<Gasto>(Gasto.class);
+	
+	//private static final Logger logger = Logger.getLogger(GastoDAO.class);
+	
+	public void adiciona(Gasto entity) {
+		dao.adiciona(entity);
 	}
 
+	public void remove(Gasto entity) {
+		dao.remove(entity);
+	}
+
+	public Gasto buscaPorId(Long id) {
+		return dao.buscaPorId(id);
+	}
+
+	public List<Gasto> buscaTodos() {
+		return dao.buscaTodos();
+	}
+	
 }
